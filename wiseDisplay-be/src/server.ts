be2214818e73c8b77e0298d17bf5b4ee165ffd64
Jonathan Korman <jonathanlkorman@gmail.com>
2023-09-04 12:22:54 -0400

    set up server

---
+++ wiseDisplay-be/src/server.ts
@@ -0,0 +1,26 @@
+import express, { Application } from 'express';
+import http from 'http';
+import bodyParser from 'body-parser';
+import compression from 'compression';
+import * as dotenv from "dotenv";
+
+import { router } from './routes';
+
+dotenv.config();
+
+const app: Application = express();
+
+const port = process.env.PORT || 8080;
+
+
+app.use(compression());
+app.use(bodyParser());
+
+const server = http.createServer(app);
+
+
+server.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}/`);
+});
+
+app.use('/api', router);
