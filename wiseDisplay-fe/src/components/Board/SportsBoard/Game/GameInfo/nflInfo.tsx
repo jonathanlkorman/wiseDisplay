aa17b3a5ea124fe91c6e593f32aaf00269d103eb
Jonathan Korman <jonathanlkorman@gmail.com>
2024-01-29 14:09:09 -0500

    handle nfl halftime

--- wiseDisplay-fe/src/components/Board/SportsBoard/Game/GameInfo/nflInfo.tsx
+++ wiseDisplay-fe/src/components/Board/SportsBoard/Game/GameInfo/nflInfo.tsx
@@ -12,10 +12,17 @@ const NFLInfo: FunctionComponent<NFLInfoData> = ({ gameData }) => {
     const gameInfo = gameData.gameInfo as IApiNFLInfo;
     return (
         <div className='game-info'>
-            <p className='quarter'>{gameInfo.quarter}</p>
-            <p className='time'>{gameData.time}</p>
-            <p className='down-and-distance'>{gameInfo.down?.replace('&', '+')}</p>
-            <p className='spot'>{gameInfo.spot}</p>
+            {gameData.detail !== 'Halftime'? (
+                <>
+                    <p className='quarter'>{gameInfo.quarter}</p>
+                    <p className='time'>{gameData.time}</p>
+                    <p className='down-and-distance'>{gameInfo.down?.replace('&', '+')}</p>
+                    <p className='spot'>{gameInfo.spot}</p>
+                </>
+            ) : (
+                <p className='quarter'>Half</p>
+            )}
+            
         </div>
     )
 }
