359ab6270aa601caf826b9895181ef58281985ce
Jonathan Korman <jonathanlkorman@gmail.com>
2024-01-31 14:07:47 -0500

    build out mlb game info

--- wiseDisplay-fe/src/components/Board/SportsBoard/Game/GameInfo/mlbInfo.tsx
+++ wiseDisplay-fe/src/components/Board/SportsBoard/Game/GameInfo/mlbInfo.tsx
@@ -1,7 +1,16 @@
 import React, { FunctionComponent } from 'react';
 
+import empty from '../../../../../assets/empty.svg'
+import first from '../../../../../assets/first.svg'
+import second from '../../../../../assets/second.svg'
+import third from '../../../../../assets/third.svg'
+import firstSecond from '../../../../../assets/first_second.svg'
+import secondThird from '../../../../../assets/second_third.svg'
+import firstThird from '../../../../../assets/first_third.svg'
+import loaded from '../../../../../assets/loaded.svg'
+
 import '../../sportsBoard.css';
-import { IApiGame } from '../../../../../../../wiseDisplay-api/interfaces/IApiGames';
+import { IApiGame, IApiMLBInfo } from '../../../../../../../wiseDisplay-api/interfaces/IApiGames';
 
 
 interface MLBInfoData {
@@ -9,10 +18,41 @@ interface MLBInfoData {
 }
 
 const MLBInfo: FunctionComponent<MLBInfoData> = ({ gameData }) => {
-    // const gameInfo = gameData.gameInfo as IApiMLBInfo;
+    const gameInfo = gameData.gameInfo as IApiMLBInfo;
+
+    const getBase = () => {
+        if (gameInfo.onFirst && gameInfo.onSecond && gameInfo.onThird)
+            return loaded;
+        else if (gameInfo.onFirst && gameInfo.onSecond && !gameInfo.onThird)
+            return firstSecond;
+        else if (gameInfo.onFirst && !gameInfo.onSecond && gameInfo.onThird)
+            return firstThird;
+        else if (!gameInfo.onFirst && gameInfo.onSecond && gameInfo.onThird)
+            return secondThird;
+        else if (!gameInfo.onFirst && !gameInfo.onSecond && gameInfo.onThird)
+            return third;
+        else if (!gameInfo.onFirst && gameInfo.onSecond && !gameInfo.onThird)
+            return second;
+        else if (gameInfo.onFirst && !gameInfo.onSecond && !gameInfo.onThird)
+            return first;
+        else 
+            return empty;
+    }
+
     return (
         <div className='game-info'>
-           
+            <p className='inning'>{gameInfo.inning}</p>
+            <img className='bases' src={getBase()} alt='bases' />
+            <p className='count'>{`${gameInfo.balls} - ${gameInfo.strikes}`}</p>
+            <div className='outs'>
+                {[...Array(3)].map((_, index) => (
+                    <div
+                        key={index}
+                        className='out'
+                        style={{ backgroundColor: !!gameInfo.outs && gameInfo.outs > index ? '#000000' : 'none' }}
+                    ></div>
+                ))}
+            </div>
         </div>
     )
 }
