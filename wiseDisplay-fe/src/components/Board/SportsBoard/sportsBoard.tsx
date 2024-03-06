77faa5a11a4e6c61c386f3413c33d496b6dfa55f
Jonathan Korman <jonathanlkorman@gmail.com>
2024-03-06 15:48:38 -0500

    refactor interval logic

--- wiseDisplay-fe/src/components/Board/SportsBoard/sportsBoard.tsx
+++ wiseDisplay-fe/src/components/Board/SportsBoard/sportsBoard.tsx
@@ -1,20 +1,21 @@
-import React, { useState, useEffect, useRef, FunctionComponent } from 'react';
+import React, { useState, useEffect } from 'react';
 import Game from './Game/game';
 import GameLoadingScreen from './Game/LoadingScreen/gameLoadingScreen';
 import useFetchGameData from '../../../hooks/useFetchGameData';
 
 interface SportsBoardProps {
-	preferences: any
+	preferences: any;
 }
 
-const SportsBoard: FunctionComponent<SportsBoardProps> = ({ preferences }) => {
+const SportsBoard: React.FC<SportsBoardProps> = ({ preferences }) => {
 	const [currentIndex, setCurrentIndex] = useState(0);
-	const { gameData, loading, fetchGameData } = useFetchGameData(preferences); // 
-	const intervalRef = useRef<NodeJS.Timeout | null>(null);
+	const { gameData, loading, fetchGameData } = useFetchGameData(preferences);
 
 	useEffect(() => {
+		let intervalId: NodeJS.Timeout | null = null;
+
 		const startInterval = () => {
-			intervalRef.current = setInterval(() => {
+			intervalId = setInterval(() => {
 				setCurrentIndex((prevIndex) => {
 					const nextIndex = (prevIndex + 1) % gameData.filteredGames.length;
 					if (nextIndex === 0) {
@@ -26,8 +27,8 @@ const SportsBoard: FunctionComponent<SportsBoardProps> = ({ preferences }) => {
 		};
 
 		const stopInterval = () => {
-			if (intervalRef.current !== null) {
-				clearInterval(intervalRef.current as NodeJS.Timeout);
+			if (intervalId !== null) {
+				clearInterval(intervalId);
 			}
 		};
 
@@ -37,19 +38,18 @@ const SportsBoard: FunctionComponent<SportsBoardProps> = ({ preferences }) => {
 			startInterval();
 		}
 
-		return () => {
-			stopInterval();
-		};
+		return stopInterval;
 	}, [fetchGameData, gameData.filteredGames.length, loading]);
 
 	if (loading) {
-		return <GameLoadingScreen />
-	} else if (gameData.filteredGames.length <= 0) {
-		return <p className='no-data'>No Games</p>;
-	} else {
-		return <Game gameData={gameData.filteredGames[currentIndex]} />;
+		return <GameLoadingScreen />;
 	}
+
+	if (gameData.filteredGames.length <= 0) {
+		return <p className="no-data">No Games</p>;
+	}
+
+	return <Game gameData={gameData.filteredGames[currentIndex]} />;
 };
 
 export default SportsBoard;
-
