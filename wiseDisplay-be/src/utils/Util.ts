eadce8e5b8dd7f408853c1eadb93c1b1244a9dda
Jonathan Korman <jonathanlkorman@gmail.com>
2024-03-09 20:26:30 -0500

    fix utc date issue

--- wiseDisplay-be/src/utils/Util.ts
+++ wiseDisplay-be/src/utils/Util.ts
@@ -5,9 +5,18 @@ export class Util {
         const params = new URLSearchParams();
       
         if (preferences.dailyOnly) {
-          params.append('dates', preferences.datetime);
+          params.append('dates', this.formatDate(preferences.datetime));
         }
       
         return params.toString() ? `?${params.toString()}` : '';
     }
+
+	public static formatDate(datetime: string): string {
+		const date =  new Date(datetime);
+		const year = date.getFullYear();
+    	const month = String(date.getMonth() + 1).padStart(2, '0');
+    	const day = String(date.getDate()).padStart(2, '0');
+
+    	return `${year}${month}${day}`;
+	}
 }
