4a1cc0661c84273504cb9b23d177c0795ebcb6b5
Jonathan Korman <jonathanlkorman@gmail.com>
2024-08-13 11:06:21 -0400

    add possession info for nfl games

--- wiseDisplay-fe/src/components/Board/SportsBoard/Game/GameInfo/nflInfo.tsx
+++ wiseDisplay-fe/src/components/Board/SportsBoard/Game/GameInfo/nflInfo.tsx
@@ -10,19 +10,37 @@ interface NFLInfoData {
 
 const NFLInfo: FunctionComponent<NFLInfoData> = ({ gameData }) => {
     const gameInfo = gameData.gameInfo as IApiNFLInfo;
+    const homePossession = gameInfo.possession === gameData.hometeam.teamShortName;
+    const awayPossession = gameInfo.possession === gameData.awayteam.teamShortName;
+
+    const styles = {
+        backgroundColor: '#ff5256',
+        fontSize: '2rem',
+        borderRadius: '8px',
+        letterSpacing: 'normal'
+    };
+
     return (
         <div className='game-info'>
-            {gameData.detail !== 'Halftime'? (
+            {gameData.detail !== 'Halftime' ? (
                 <>
                     <span className='quarter'>{gameInfo.quarter}</span>
                     <span className='time'>{gameData.time}</span>
                     <span className='down-and-distance'>{gameInfo.down}</span>
                     <span className='spot'>{gameInfo.spot}</span>
+                    {(homePossession || awayPossession) &&
+                        <span className={`possession possession-${homePossession ? `home` : `away`}`}>
+                            <span className='possession-icon' style={gameInfo.redzone ? styles : {}}>
+                                {gameInfo.redzone ? "RZ" : "--|--|--|--|--"}
+                            </span>
+                        </span>
+                    }
+
                 </>
             ) : (
                 <span className='quarter'>Half</span>
             )}
-            
+
         </div>
     )
 }
